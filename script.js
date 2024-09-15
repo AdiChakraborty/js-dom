7b26543774387f76aace39ba754f285964c4242c
Adi chakraborty <adi.chakraborty.007@gmail.com>
2024-09-15 10:29:10 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// console.log("Adi");
+
+
+// let boxes =document.getElementsByClassName("box");
+// console.log(boxes);
+
+// boxes[2].style.backgroundColor= "aqua";
+// boxes[3].style.backgroundColor= "purple";
+// boxes[4].style.backgroundColor= "gray";
+
+
+// document.getElementById("red").style.backgroundColor= "red";
+
+// document.querySelector(".box").style.backgroundColor="aqua";
+
+// document.querySelectorAll(".box").forEach(e=>{
+// e.style.backgroundColor="aqua";
+
+// })
+
+
+////////////////////////solution of the practice ///////////////////////////////    
+
+// let a = 6;
+// function factorial(number){
+//     let arr = Array.from(Array(number+1).keys())
+//     // console.log(arr.slice(1,));
+//     let c = arr.slice(1,).reduce((a , b)=>{
+//         return a * b 
+        
+//     })
+//     return c;
+//     // console.log(c);
+// }
+
+// factorial(a)
+
+
+// document.querySelector(".container").innertext
